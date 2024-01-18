1006b848ce61135506c85099268ffb62e1720c24
Azul Garcia <garcia.azul.maria@gmail.com>
2024-01-18 20:40:21 +0100

    function return_of_invesment

--- main.py
+++ main.py
@@ -2,21 +2,63 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+def calculate_performances_many_years(projects_all, list_dates, top):
+    """
+    - return mean performance of wallet joining different years.
+
+        Args:
+        - projects_all: normally 1000$.
+        - list_dates: to calculate the accumulated.
+        - top_project: wallet size.
+
+        Returns:
+        - project_score from the top_position (ideally top 10).
+    """
+    performance_total = [0]
+    for date_i in list_dates[:-1].copy():
+        year = date_i.year
+        week = date_i.isocalendar()[1]
+        projects_in_year = projects_all[projects_all['year'] == year]
+        top_projects = projects_in_year[projects_in_year['week'] == week].iloc[0:top, :]
+        performance_total.append(top_projects['performance'].mean(skipna=True))
+    return performance_total
+
+def return_of_invesment(init_investment, list_performance):
+    """
+    - Gets ROI of invesment in a ordered list of performance.
+
+        Args:
+        - init_investment: normally 1000$.
+        - list_performance: to calculate the accumulated
+
+        Returns:
+        - project_score from the top_position (ideally top 10).
+    """
+    accumulated_roi = init_investment
+    roi_total = []
+    for perf in list_performance:
+        accumulated_roi = round(accumulated_roi * (1 + perf), 2)
+        roi_total.append(accumulated_roi)
+    return roi_total
+
+
+
 df = pd.read_csv('all_categories_performance.csv')
 df['date'] = pd.to_datetime(df['date'])
 
 ###############################################################################################################
 # Performance for date
 ###############################################################################################################
-grouped_df = df.groupby(['category', 'date']).agg({'performance': 'sum'}).reset_index()
+#grouped_df = df.groupby(['category', 'date']).agg({'performance': 'sum'}).reset_index()
 
 category_filter = ['Gems', 'Gems<15', 'Gems<25']
-filtered_df = grouped_df[grouped_df['category'].isin(category_filter)]
+filtered_df = df[df['category'].isin(category_filter)]
 
 year_filter = [2023, 2024]
 filtered_df = filtered_df[filtered_df['date'].dt.year.isin(year_filter)]
 filtered_df['date'] = pd.to_datetime(filtered_df['date']).dt.date
-print(filtered_df)
+
+print(filtered_df.columns)
 
 ###############################################################################################################
 # All projects for year and week
@@ -30,9 +72,6 @@ print(all_gems_df)
 ###############################################################################################################
 
 st.title('Performance Gems')
-#categorias = filtered_df['category'].unique()
-#categoria_seleccionada = st.sidebar.selectbox('Selecciona una categoría', categorias)
-#df_filtrado = filtered_df[filtered_df['category'] == categoria_seleccionada]
 
 rango_fechas = pd.date_range(df['date'].min(), df['date'].max())
 fecha_inicio_default = max(rango_fechas.min().date(), filtered_df['date'].min())  # Establecer un valor predeterminado dentro del rango
@@ -46,11 +85,16 @@ fecha_fin = st.date_input("Select end date",
                           max_value=rango_fechas.max().date(),
                           value=fecha_fin_default)
 
-df_filtrado =filtered_df[(filtered_df['date'] >= fecha_inicio) & (filtered_df['date'] <= fecha_fin)]
-fig = px.line(df_filtrado, x='date', y='performance', color='category', markers=True,
-              labels={'performance': 'Performance', 'date': 'Date'},)
-st.plotly_chart(fig)
+df_filtrado = filtered_df[(filtered_df['date'] >= fecha_inicio) & (filtered_df['date'] <= fecha_fin)]
 
+df_filtrado = df_filtrado.groupby(['category', 'date'])['performance'].mean().reset_index()
+
+df_filtrado['return_total'] = return_of_invesment(1000, df_filtrado['performance'].tolist())
+
+fig_roi = px.line(df_filtrado, x='date', y='return_total', color='category', markers=True,
+                  labels={'performance': 'Mean Performance', 'date': 'Date'},)
+
+st.plotly_chart(fig_roi)
 
 st.title('Projects by category and date')
 categorias = all_gems_df['category'].unique()
@@ -69,4 +113,4 @@ filtered_gems = all_gems_df[(all_gems_df['category'] == categoria_seleccionada)
 
 columnas_a_mostrar = ['category', 'date', 'project_name', 'start_price', 'end_price', 'performance']
 
-st.write(filtered_gems[columnas_a_mostrar], index=False)
+st.write(filtered_gems[columnas_a_mostrar])
