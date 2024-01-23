d24564e4b742709e0178efafeba1b3062ea2a991
Azul Garcia <garcia.azul.maria@gmail.com>
2024-01-23 13:57:07 +0100

    performances top 30 general

---
+++ get_price_top_30.py
@@ -0,0 +1,123 @@
+from cb_database_connection import open_connection
+import pandas as pd
+
+connection = open_connection()
+cursor = connection.cursor(dictionary=True)
+
+all_projects_df = pd.read_csv("all_projects_top_39_3.csv")
+
+id_project_list = list(set(all_projects_df['id_project'].to_list()))
+
+data = []
+
+## 2024
+year = 2024
+week = 1
+for id in id_project_list:
+    try:
+        query_init_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                   "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+        cursor.execute(query_init_price, (2023, 52, id))
+        price_init_week = cursor.fetchall()
+        init_price = float(price_init_week[0]['price'])
+
+        query_end_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                  "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+        cursor.execute(query_end_price, (year, week-1, id))
+        price_last_week = cursor.fetchall()
+        end_price = float(price_last_week[0]['price'])
+
+        performance = (end_price - init_price) / init_price
+
+        project_name = all_projects_df.loc[all_projects_df['id_project'] == id, 'name'].iloc[0]
+
+        data.append({'category': 'General',
+                 'date': price_last_week[0]['created_at'],
+                 'year': year,
+                 'week': week,
+                 'id_project': id,
+                 'project_name': project_name,
+                 'start_price': init_price,
+                 'end_price': end_price,
+                 'performance': performance
+                 })
+        print('General', price_last_week[0]['created_at'], year, week, id, project_name,
+              init_price, end_price, performance)
+    except:
+        print(f'{year}, {week}, Project: {project_name},: has prices problems')
+
+## 2024 others weeks
+year = 2024
+for id in id_project_list:
+    for week in range(2, 4):
+        try:
+            query_init_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                       "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+            cursor.execute(query_init_price, (year, week-2, id))
+            price_init_week = cursor.fetchall()
+            init_price = float(price_init_week[0]['price'])
+
+            query_end_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                      "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+            cursor.execute(query_end_price, (year, week-1, id))
+            price_last_week = cursor.fetchall()
+            end_price = float(price_last_week[0]['price'])
+
+            performance = (end_price - init_price) / init_price
+
+            project_name = all_projects_df.loc[all_projects_df['id_project'] == id, 'name'].iloc[0]
+
+            data.append({'category': 'General',
+                     'date': price_last_week[0]['created_at'],
+                     'year': year,
+                     'week': week,
+                     'id_project': id,
+                     'project_name': project_name,
+                     'start_price': init_price,
+                     'end_price': end_price,
+                     'performance': performance
+                     })
+
+            print('General', price_last_week[0]['created_at'], year, week, id, project_name,
+                  init_price, end_price, performance)
+        except:
+            print(f'{year}, {week}, Project: {project_name},: has prices problems')
+
+## 2024 others weeks
+year = 2023
+for id in id_project_list:
+    for week in range(39, 53):
+        try:
+            query_init_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                       "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+            cursor.execute(query_init_price, (year, week-1, id))
+            price_init_week = cursor.fetchall()
+            init_price = float(price_init_week[0]['price'])
+
+            query_end_price = "select price, DATE(created_at) as created_at from cryptobirds.prices " \
+                                                      "where year(updated_at) = %s and week(updated_at) = %s and id_project = %s"
+            cursor.execute(query_end_price, (year, week, id))
+            price_last_week = cursor.fetchall()
+            end_price = float(price_last_week[0]['price'])
+
+            performance = (end_price - init_price) / init_price
+
+            project_name = all_projects_df.loc[all_projects_df['id_project'] == id, 'name'].iloc[0]
+
+            data.append({'category': 'General',
+                     'date': price_last_week[0]['created_at'],
+                     'year': year,
+                     'week': week,
+                     'id_project': id,
+                     'project_name': project_name,
+                     'start_price': init_price,
+                     'end_price': end_price,
+                     'performance': performance
+                     })
+
+            print('General', price_last_week[0]['created_at'], year, week, id, project_name,
+                  init_price, end_price, performance)
+        except:
+            print(f'{year}, {week}, Project: {project_name},: has prices problems')
+
+pd.DataFrame(data).to_csv('top_30_performances_39_3.csv', sep=',', index=False)
---
+++ top_30_projects.py
@@ -0,0 +1,42 @@
+from cb_database_connection import open_connection, close_connection
+import pandas as pd
+
+def fetch_data (connection, year, week):
+    query_end_price = "SELECT id_project, name, top, score, year, week FROM cryptobirds.projects_scores as a left join cryptobirds.projects as b ON a.id_project = b.id where a.year = %s and a.week = %s and a.top = 'General' order by a.score desc limit 30;"
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query_end_price, (year, week))
+
+    return pd.DataFrame(cursor.fetchall())
+
+
+all_projects_df = pd.read_csv("all_projects_top_30.csv")
+
+project_week_counts = all_projects_df.groupby('name')['week'].nunique().reset_index()
+
+project_week_counts = project_week_counts.rename(columns={'week': 'weeks_count'})
+
+#bitcoin = project_week_counts[project_week_counts['name'] == 'Bitcoin']
+
+project_week_counts_sorted = project_week_counts.sort_values(by='weeks_count', ascending=False)
+print(project_week_counts_sorted)
+
+'''
+connection = open_connection()
+
+all_prorjects_df = pd.DataFrame()
+
+### projects year 2024
+for week in range(1, 4):
+    projects = fetch_data(connection, 2024, week)
+    all_prorjects_df = pd.concat([all_prorjects_df, projects], ignore_index=True)
+
+### projects year 2023
+for week in range(24, 53):
+    projects = fetch_data(connection, 2023, week)
+    all_prorjects_df = pd.concat([all_prorjects_df, projects], ignore_index=True)
+
+print(all_prorjects_df)
+all_prorjects_df.to_csv("all_projects_top_30.csv")
+connection.close()
+'''
